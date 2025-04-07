ee05450bfaf9fe2368a08ff7837e1ce0821b6751
EmilySouza22 <emilyseafonso@gmail.com>
2025-04-06 22:56:15 -0300

    Altera código js

--- aula03/script03.js
+++ aula03/script03.js
@@ -1,19 +1,21 @@
-let rodada = 1;
-while(rodada <= 3) {
-  console.log("rodada " + rodada);
-  
-  let escolhaJogador = prompt("Nível " + rodada + ", escolha vidro (1), (2) ou (3): ");
-  let pisoQuebrado = Math.floor(Math.random()* 3) + 1;
-  
-  if(escolhaJogador == pisoQuebrado){
-    alert("Ops, você pisou no vidro errado! Game Over...")
-    rodada = 1000;
-  } else {
-    alert("Passou! Piso quebrado estava na ponte " + pisoQuebrado)
-  }
-  rodada = rodada + 1;
-}
-
-if(rodada == 4){
-  alert("Parabéns, você venceu o jogo!")
+function jogar(){       
+    let rodada = 1;
+    while(rodada <= 5) {
+      console.log("rodada " + rodada);
+    
+      let escolhaJogador = prompt("Nível " + rodada + ", escolha vidro (1), (2) ou (3): ");
+      let pisoQuebrado = Math.floor(Math.random()* 3) + 1;
+    
+      if(escolhaJogador == pisoQuebrado){
+        alert("Ops, você pisou no vidro errado! Game Over...")
+        rodada = 1000;
+      } else {
+        alert("Passou! Piso quebrado estava na ponte " + pisoQuebrado)
+      }
+      rodada = rodada + 1;
+    }
+    
+    if(rodada == 6){
+      alert("Parabéns, você venceu o jogo!")
+    }
 }
