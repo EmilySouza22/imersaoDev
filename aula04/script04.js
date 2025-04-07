e70ff1ad27f3b728732fa03882174bd84551ae90
EmilySouza22 <emilyseafonso@gmail.com>
2025-04-07 00:58:15 -0300

    Adiciona a ação do botão

--- aula04/script04.js
+++ aula04/script04.js
@@ -1,39 +1,41 @@
-//Preciso criar uma lista com os 3 jogadores
-personagem = ["","",""];
-viloes = ["","",""];
+function jogar(){
+    //Preciso criar uma lista com os 3 jogadores
+    personagem = ["","",""];
+    viloes = ["","",""];
 
-forcaPersonagem = 0;
-forcaViloes = 0;
+    forcaPersonagem = 0;
+    forcaViloes = 0;
 
-for(let i=0;i<3; i++){
-    escolhaPersonagem = prompt("Digite o nome do seu personagem!" + (i + 1));
-    personagem[i] = escolhaPersonagem;
-    
-    //Calcular a força de cada jogador, e depois somar pra saber a força do time
-    //forcaPersonagem = forcaPersonagem + Math.floor(Math.random() * 10) + 1; (Opção 1)
-    forcaPersonagem += Math.floor(Math.random() * 10) + 1;
-}
-console.log("A array personagem contém: " + personagem);
+    for(let i=0;i<3; i++){
+        escolhaPersonagem = prompt("Digite o nome do seu personagem! " + (i + 1));
+        personagem[i] = escolhaPersonagem;
+        
+        //Calcular a força de cada jogador, e depois somar pra saber a força do time
+        //forcaPersonagem = forcaPersonagem + Math.floor(Math.random() * 10) + 1; (Opção 1)
+        forcaPersonagem += Math.floor(Math.random() * 10) + 1;
+    }
+    console.log("A array personagem contém: " + personagem);
 
-for(let i=0; i<3; i++){
-    indiceAleatorio = Math.floor(Math.random() * 10);
-    viloesPossiveis = ["Darth Vader", "Palpatine", "Darth Maul", "Conde Dookan", "General Grievous", 
-    "Asajj Ventress", "Jabba The Hutt", "Grande Inquisidor", "Almirante Thrawn", "Moof Tarkin"]
-    viloes[i] = viloesPossiveis[indiceAleatorio]
-    
-    //Calcular a força de cada jogador do time do computador
-    forcaViloes += Math.floor(Math.random() * 10) + 1;
-}
-console.log("Vilões: " + viloes)
+    for(let i=0; i<3; i++){
+        indiceAleatorio = Math.floor(Math.random() * 10);
+        viloesPossiveis = ["Darth Vader", "Palpatine", "Darth Maul", "Conde Dookan", "General Grievous", 
+        "Asajj Ventress", "Jabba The Hutt", "Grande Inquisidor", "Almirante Thrawn", "Moof Tarkin"]
+        viloes[i] = viloesPossiveis[indiceAleatorio]
+        
+        //Calcular a força de cada jogador do time do computador
+        forcaViloes += Math.floor(Math.random() * 10) + 1;
+    }
+    console.log("Vilões: " + viloes)
 
-//Comparar os dois times para saber quem venceu.
+    //Comparar os dois times para saber quem venceu.
 
-if(forcaPersonagem > forcaViloes){
-    alert("Seu time é mais forte! Você venceu! Sua força foi " + forcaPersonagem)
-} else {
-    if(forcaPersonagem < forcaViloes){
-        alert("O time dos vilões é mais forte! A força dos vilões foi " + forcaViloes)
+    if(forcaPersonagem > forcaViloes){
+        alert("Seu time é mais forte! Você venceu! Sua força foi " + forcaPersonagem)
     } else {
-        alert("Os dois times tem a mesma força, deu empate!")
+        if(forcaPersonagem < forcaViloes){
+            alert("O time dos vilões é mais forte! A força dos vilões foi " + forcaViloes)
+        } else {
+            alert("Os dois times tem a mesma força, deu empate!")
+        }
     }
 }
